d72a9afa4defe89cafb93254c6d586395d73b04f
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-08-19 01:52:51 +0600

    Add files via upload

---
+++ Crud Application/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Crud_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.IDtb = new System.Windows.Forms.TextBox();
+            this.NAMEtb = new System.Windows.Forms.TextBox();
+            this.AGEtb = new System.Windows.Forms.TextBox();
+            this.ID = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // IDtb
+            // 
+            this.IDtb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.IDtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IDtb.Location = new System.Drawing.Point(695, 94);
+            this.IDtb.Name = "IDtb";
+            this.IDtb.Size = new System.Drawing.Size(387, 35);
+            this.IDtb.TabIndex = 0;
+            // 
+            // NAMEtb
+            // 
+            this.NAMEtb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NAMEtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NAMEtb.Location = new System.Drawing.Point(695, 186);
+            this.NAMEtb.Name = "NAMEtb";
+            this.NAMEtb.Size = new System.Drawing.Size(387, 35);
+            this.NAMEtb.TabIndex = 1;
+            // 
+            // AGEtb
+            // 
+            this.AGEtb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AGEtb.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AGEtb.Location = new System.Drawing.Point(695, 283);
+            this.AGEtb.Name = "AGEtb";
+            this.AGEtb.Size = new System.Drawing.Size(387, 35);
+            this.AGEtb.TabIndex = 2;
+            // 
+            // ID
+            // 
+            this.ID.AutoSize = true;
+            this.ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID.Location = new System.Drawing.Point(554, 90);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(50, 37);
+            this.ID.TabIndex = 1;
+            this.ID.Text = "ID";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(554, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 37);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(554, 283);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Age";
+            // 
+            // button1
+            // 
+            this.button1.AutoEllipsis = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(561, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 52);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Insert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(682, 368);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Delete";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Yellow;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(803, 368);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 52);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Cyan;
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(942, 368);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(133, 52);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(561, 442);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(521, 150);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label3.Font = new System.Drawing.Font("Montserrat ExtraBold", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(461, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 44);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "CRUD APP";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-44, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(530, 603);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 617);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.AGEtb);
+            this.Controls.Add(this.NAMEtb);
+            this.Controls.Add(this.IDtb);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "CRUD APPLICATION BY CODE CRAFT ZOBAER";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IDtb;
+        private System.Windows.Forms.TextBox NAMEtb;
+        private System.Windows.Forms.TextBox AGEtb;
+        private System.Windows.Forms.Label ID;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+    }
+}
+
---
+++ Crud Application/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crud_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string conString = "Data Source=ZOBAER;Initial Catalog=CrudApplication;Integrated Security=True;TrustServerCertificate=True";
+        //Insert
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("INSERT INTO CATable(id, name, age) VALUES(@id, @name, @age)", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(IDtb.Text));
+            cmd.Parameters.AddWithValue("@name",NAMEtb.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(AGEtb.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Inserted");
+
+            if (string.IsNullOrEmpty(IDtb.Text) == true)
+            {
+                IDtb.Focus();
+                errorProvider1.SetError(this.IDtb, "Please fill ID");
+            }
+            else if(string.IsNullOrEmpty(NAMEtb.Text) == true)
+            {
+                NAMEtb.Focus();
+                errorProvider2.SetError(this.NAMEtb, "Please fill Name");
+            }
+            else if (string.IsNullOrEmpty(AGEtb.Text) == true)
+            {
+                AGEtb.Focus();
+                errorProvider3.SetError(this.AGEtb, "Please fill Age");
+            }
+            else
+            {
+                errorProvider1.Clear();
+                errorProvider2.Clear();
+                errorProvider3.Clear();
+            }
+        }
+        //Delete
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("DELETE CATable WHERE id=@id",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(IDtb.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Deleted");
+        }
+        //Update
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("UPDATE CATable SET name=@name, age=@age WHERE id=@id",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(IDtb.Text)); 
+            cmd.Parameters.AddWithValue("@name", NAMEtb.Text);
+            cmd.Parameters.AddWithValue("@age", double.Parse(AGEtb.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Successfully Updated");
+
+            if (string.IsNullOrEmpty(IDtb.Text) == true)
+            {
+                IDtb.Focus();
+                errorProvider1.SetError(this.IDtb, "Please fill ID");
+            }
+            else if (string.IsNullOrEmpty(NAMEtb.Text) == true)
+            {
+                NAMEtb.Focus();
+                errorProvider2.SetError(this.NAMEtb, "Please fill Name");
+            }
+            else if (string.IsNullOrEmpty(AGEtb.Text) == true)
+            {
+                AGEtb.Focus();
+                errorProvider3.SetError(this.AGEtb, "Please fill Age");
+            }
+            else
+            {
+                errorProvider1.Clear();
+                errorProvider2.Clear();
+                errorProvider3.Clear();
+            }
+        }
+        //Search
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM CATable WHERE id=@id",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(IDtb.Text));
+            SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            dataAdapter.Fill(dt);
+
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
